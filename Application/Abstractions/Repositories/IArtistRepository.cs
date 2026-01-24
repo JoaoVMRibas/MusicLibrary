cff2987148c6d5dccea9d6642b91f9c446e865da
João Ribas <joaovitormribas@gmail.com>
2026-01-24 16:01:40 -0300

    Adding the artist repository interface

---
+++ Application/Abstractions/Repositories/IArtistRepository.cs
@@ -0,0 +1,12 @@
+﻿using MusicLibrary.Domain.Entities;
+
+namespace MusicLibrary.Application.Abstractions.Repositories;
+
+public interface IArtistRepository
+{
+    Task<Artist?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Artist>> GetAllAsync();
+    Task AddAsync(Artist artist);
+    Task UpdateAsync(Artist artist);
+    Task DeleteAsync(Artist artist);
+}
