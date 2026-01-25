f4ce4f2408303808a2a4e4d8c57b57fff4ff72c8
João Ribas <joaovitormribas@gmail.com>
2026-01-25 12:30:04 -0300

    Adding AlbumService and adjustments to IAlbumService

--- MusicLibrary.Application/Abstractions/Services/IAlbumService.cs
+++ MusicLibrary.Application/Abstractions/Services/IAlbumService.cs
@@ -6,8 +6,8 @@ namespace MusicLibrary.Application.Abstractions.Services;
 
 public interface IAlbumService
 {
-    Task<AlbumDto> CreateAlbumAsync(CreateAlbumRequest request);
+    Task<AlbumDto> CreateAlbumAsync(Guid artistId, CreateAlbumRequest request);
     Task<AlbumDto?> GetAlbumByIdAsync(Guid artistId,Guid albumId);
-    Task<IReadOnlyCollection<Album>> GetAlbumsByArtist(Guid artistId);
+    Task<IReadOnlyCollection<AlbumDto>> GetAlbumsByArtist(Guid artistId);
     Task DeleteAlbumAsync(Guid artistId,Guid albumId);
 }
---
+++ MusicLibrary.Application/Services/AlbumService.cs
@@ -0,0 +1,52 @@
+﻿using MusicLibrary.Application.Abstractions.Repositories;
+using MusicLibrary.Application.Abstractions.Services;
+using MusicLibrary.Application.Requests;
+using MusicLibrary.Application.Responses.DTOs;
+
+namespace MusicLibrary.Application.Services;
+
+public class AlbumService : IAlbumService
+{
+    private readonly IArtistRepository _artistRepository;
+
+    public AlbumService(IArtistRepository artistRepository)
+    {
+        _artistRepository = artistRepository;
+    }
+
+    public async Task<AlbumDto> CreateAlbumAsync(Guid artistId, CreateAlbumRequest request)
+    {
+        var artist = await _artistRepository.GetByIdAsync(artistId) ?? throw new InvalidOperationException("Artist not found.");
+
+        var album = artist.AddAlbum(request.Name);
+
+        await _artistRepository.UpdateAsync(artist);
+
+        return new AlbumDto(album.Id, album.Name,album.Duration);
+    }
+
+    public async Task<AlbumDto?> GetAlbumByIdAsync(Guid artistId, Guid albumId)
+    {
+        var artist = await _artistRepository.GetByIdAsync(artistId) ?? throw new InvalidOperationException("Artist not found.");
+        
+        var album = artist.Albums.FirstOrDefault(a =>  a.Id == albumId) ?? throw new InvalidOperationException("Album not found.");
+
+        return new AlbumDto(album.Id,album.Name,album.Duration);
+    }
+
+    public async Task<IReadOnlyCollection<AlbumDto>> GetAlbumsByArtist(Guid artistId)
+    {
+        var artist = await _artistRepository.GetByIdAsync(artistId) ?? throw new InvalidOperationException("Artist not found.");
+
+        return artist.Albums.Select(a => new AlbumDto(a.Id,a.Name,a.Duration)).ToList();
+    }
+
+    public async Task DeleteAlbumAsync(Guid artistId, Guid albumId)
+    {
+        var artist = await _artistRepository.GetByIdAsync(artistId) ?? throw new InvalidOperationException("Artist not found.");
+
+        artist.RemoveAlbum(albumId);
+
+        await _artistRepository.UpdateAsync(artist);
+    }
+}
