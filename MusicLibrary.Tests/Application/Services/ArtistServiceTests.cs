7252dddf790ab3616d7bf49fb156dad3c3e277d6
João Ribas <joaovitormribas@gmail.com>
2026-01-24 21:22:07 -0300

    Correcting directory and reference problems

---
+++ MusicLibrary.Tests/Application/Services/ArtistServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using MusicLibrary.Application.Abstractions.Repositories;
+using NSubstitute;
+
+namespace MusicLibrary.Tests.Application.Services;
+
+public class ArtistServiceTests
+{
+    [Fact]
+    public async Task Should_Create_Artist_With_Valid_Name()
+    {
+        //Arrange
+        var repository = Substitute.For<IArtistRepository>();
+    }
+}
