e756d8d0b3623f3440445efcd5c72028eded8ffa
João Ribas <joaovitormribas@gmail.com>
2026-02-02 22:54:46 -0300

    Adding ArtistController and using Swagger

---
+++ MusicLibrary.WebAPI/Controllers/ArtistsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicLibrary.Application.Abstractions.Services;
+using MusicLibrary.Application.Requests.Artist;
+
+namespace MusicLibrary.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ArtistsController : ControllerBase
+{
+    private readonly IArtistService _artistService;
+    
+    public ArtistsController(IArtistService artistService) 
+    {  
+        _artistService = artistService; 
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateArtistRequest request)
+    {
+        var artist = await _artistService.CreateAsync(request);
+
+        return CreatedAtAction(
+        nameof(GetById),
+        new { id = artist.Id },
+        artist
+        );
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var artists = await _artistService.GetAllAsync();
+        
+        return Ok(artists);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id) 
+    {
+        var artist = await _artistService.GetByIdAsync(id);
+
+        return Ok(artist);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(Guid id,[FromBody] UpdateArtistRequest request)
+    {
+        var artist = await _artistService.UpdateAsync(id,request);
+
+        return Ok(artist);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id) 
+    {
+        await _artistService.DeleteAsync(id);
+
+        return NoContent();
+    }
+}
--- MusicLibrary.WebAPI/Program.cs
+++ MusicLibrary.WebAPI/Program.cs
@@ -7,16 +7,27 @@ using MusicLibrary.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
-builder.Services.AddScoped<IArtistService, ArtistService>();
-builder.Services.AddScoped<IAlbumService, AlbumService>();
-builder.Services.AddScoped<IMusicService, MusicService>();
 builder.Services.AddDbContext<MusicLibraryDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
+
+builder.Services.AddScoped<IArtistService, ArtistService>();
+builder.Services.AddScoped<IAlbumService, AlbumService>();
+builder.Services.AddScoped<IMusicService, MusicService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
