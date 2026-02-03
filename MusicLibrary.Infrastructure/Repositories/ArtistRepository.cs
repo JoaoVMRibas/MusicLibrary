335ef8266dddb9d44dc48ba4351e40ef279707d1
João Ribas <joaovitormribas@gmail.com>
2026-02-02 21:46:01 -0300

    Adding the implementation of the ArtistRepository class

---
+++ MusicLibrary.Infrastructure/Repositories/ArtistRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibrary.Application.Abstractions.Repositories;
+using MusicLibrary.Domain.Entities;
+using MusicLibrary.Infrastructure.Persistence;
+
+namespace MusicLibrary.Infrastructure.Repositories;
+
+public class ArtistRepository : IArtistRepository
+{
+    private readonly MusicLibraryDbContext _context;
+    public ArtistRepository(MusicLibraryDbContext context)
+    {
+        _context = context;
+    }
+    public async Task AddAsync(Artist artist)
+    {
+        await _context.Artists.AddAsync(artist);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Artist?> GetByIdAsync(Guid id)
+    {
+        return await _context.Artists
+                        .Include(a => a.Albums)
+                            .ThenInclude(al => al.Musics)
+                        .Include(a => a.Musics)
+                        .FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<IReadOnlyCollection<Artist>> GetAllAsync()
+    {
+        return await _context.Artists
+                        .Include(a => a.Albums)
+                        .Include(a => a.Musics)
+                        .ToListAsync();
+    }
+
+    public async Task UpdateAsync(Artist artist)
+    {
+        _context.Artists.Update(artist);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Artist artist)
+    {
+        _context.Artists.Remove(artist);
+        await _context.SaveChangesAsync();
+    }
+}
--- MusicLibrary.WebAPI/Program.cs
+++ MusicLibrary.WebAPI/Program.cs
@@ -1,9 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using MusicLibrary.Application.Abstractions.Repositories;
+using MusicLibrary.Application.Abstractions.Services;
+using MusicLibrary.Application.Services;
 using MusicLibrary.Infrastructure.Persistence;
+using MusicLibrary.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
+builder.Services.AddScoped<IArtistService, ArtistService>();
+builder.Services.AddScoped<IAlbumService, AlbumService>();
+builder.Services.AddScoped<IMusicService, MusicService>();
 builder.Services.AddDbContext<MusicLibraryDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
